893eb83df5de31edf02637c1a7adee5aad54c28a
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-09 14:57:00 +0900

    worksテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_051233) do
+ActiveRecord::Schema.define(version: 2021_10_09_054226) do
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2021_10_09_051233) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "works", charset: "utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
   end
 
 end
