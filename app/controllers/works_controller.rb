a6340cfb758c8867e8ac9bc659b2414195e5e645
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-09 16:07:13 +0900

    createアクション追加

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,13 +1,23 @@
 class WorksController < ApplicationController
 
+  def index
 
   def new
     @work = Work.new
   end
 
+  def create
+    @record = Work.new(work_params)
+    if @record.save
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
   private
 
-  def message_params
+  def work_params
     params.require(:work).permit(:title, :text, :image)
   end
 end
