af2813a902e1ec8b88f0b965dd1496020b622290
Taiki Noda <taiki.nd.real@gmail.com>
2021-10-11 20:18:20 +0900

    jQueryの実装

--- app/javascript/packs/fade.js
+++ app/javascript/packs/fade.js
@@ -39,4 +39,32 @@ $(function(){
     });
   });
 
+
+
+  // アニメーションさせたいクラス
+  var container = $(".title_word");
+  // アニメーションスピード
+  var speed = 80;
+
+  // テキストの間にスペースを入れます
+  var content = container.html();
+  var text = $.trim(content);
+  var newHtml = "";
+
+  // スペースで区切ったテキストを、テキストの数だけspanで囲む
+  text.split("").forEach(function(v) {
+    newHtml += '<span>' + v + '</span>';
+  });
+
+  // spanで囲んだテキスト群をHTMLに戻す
+  container.html(newHtml);
+
+  // 1文字ずつ表示
+  var txtNum = 0;
+  container.css({opacity: 1});
+  setInterval(function() {
+    container.find('span').eq(txtNum).css({opacity: 1});
+    txtNum++
+  }, speed);
+
 });
