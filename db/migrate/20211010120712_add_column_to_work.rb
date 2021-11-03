cf20fb6e99bd3a08b64bc1f545e702aa1215dc8e
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-03 19:21:55 +0900

    修正

--- db/migrate/20211010120712_add_column_to_work.rb
+++ db/migrate/20211010120712_add_column_to_work.rb
@@ -1,7 +1,5 @@
 class AddColumnToWork < ActiveRecord::Migration[6.1]
   def change
     add_column :works, :url, :string
-    add_column :works, :url2, :string
-    add_column :works, :url3, :string
   end
 end
