13fa3d82dc99d476f91872f74dc84b841e76f8c8
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-21 14:28:58 +0900

    markdownの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
   root to: 'works#index'
   resources :works
+  post 'posts/attach', to: 'posts#attach'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_102723) do
+ActiveRecord::Schema.define(version: 2022_01_21_052529) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,10 +40,22 @@ ActiveRecord::Schema.define(version: 2021_11_03_102723) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "attachments", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "languages", charset: "utf8", force: :cascade do |t|
     t.string "langage"
     t.integer "point"
     t.string "kind"
+  end
+
+  create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
